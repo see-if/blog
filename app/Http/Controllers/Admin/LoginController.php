c4e7a6bd322dfbc10329af76573a15f69fa8ab8d
liner <2259601853@qq.com>
2020-08-11 20:41:17 +0800

    新增登录页面

---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LoginController extends Controller
+{
+    //登录页面
+    public function login(){
+        return view("admin.login");
+    }
+}
